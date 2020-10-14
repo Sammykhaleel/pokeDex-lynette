5b70d8c76e2e5511146f942d24e3771d66b0ce0c
LS9834 <72521089+LS9834@users.noreply.github.com>
2020-10-14 12:50:04 -0500

    1.2newvariable/array/objects

--- JS/scripts.js
+++ JS/scripts.js
@@ -1,3 +1,4 @@
-alert('Hello world');
-let text="Hello There"
-document.write(text)
+var pokemonList 
+let name = [Squirtle, Butterfree , Vaporeon];
+let height = [7, 12, 134];
+let types = [water, bug, water];
