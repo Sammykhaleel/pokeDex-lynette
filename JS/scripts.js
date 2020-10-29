ee39425a3a88a1757a3fc93dbbbb3dc2efc8289b
LS9834 <72521089+LS9834@users.noreply.github.com>
2020-10-29 10:51:19 -0500

    1.7

--- JS/scripts.js
+++ JS/scripts.js
@@ -1,77 +1,104 @@
 let pokemonRepository = (function () {
-
-    let pokemonList = [
-        { name: "Squirtle", height: 7, types: ["grass", "slow"] },
-        { name: "Butterfree", height: 10, types: ["bug", "flying"] },
-        { name: "Vaporeon", height: 15, types: ["water", "fast"] },
-        { name: "Evy", height: 11, types: ["speed", "electric"] },
-        { name: "Charmander", height: 16, types: ["fire", "flying"] },
-      ];
-
-      function add(pokemon) {
-        if (
-          typeof pokemon === "object" &&
-          "name" in pokemon &&
-          "height" in pokemon &&
-          "types" in pokemon
-        ) {
-          pokemonList.push(pokemon);
-        }
-      }
-  
-    function getAll() {
-      return pokemonList;
+  let pokemonList = [];
+  let apiUrl = "https://pokeapi.co/api/v2/pokemon/?limit=150";
+  function add(pokemon) {
+    if (typeof pokemon === "object") {
+      pokemonList.push(pokemon);
     }
-  
-    function addlistitem (pokemon){
-      let ul=document.querySelector(".pokemon-list"); 
-      let li=document.createElement("li");
-      let button=document.createElement("button");
-      button.innerText=pokemon.name;
-      button.classList.add("button")
-      li.appendChild(button);
-      ul.appendChild(li);
-      button.addEventListener("click",function(event){
-     showDetails(pokemon)
+  }
+
+  function getAll() {
+    return pokemonList;
+  }
+
+  function addlistitem(pokemon) {
+    let ul = document.querySelector(".pokemon-list");
+    let li = document.createElement("li");
+    let button = document.createElement("button");
+    button.innerText = pokemon.name;
+    button.classList.add("button");
+    li.appendChild(button);
+    ul.appendChild(li);
+    button.addEventListener("click", function (event) {
+      showDetails(pokemon);
+    });
+  }
+
+  function showDetails(pokemon) {
+    loadDetails(pokemon).then(function(){
+      console.log(pokemon);
+    })
+    
+  }
+
+  function loadList() {
+    return fetch(apiUrl)
+      .then(function (response) {
+        return response.json();
       })
-    }
+      .then(function (json) {
+        json.results.forEach(function (item) {
+          let pokemon = {
+            name: item.name,
+            detailsUrl: item.url,
+          };
+          add(pokemon);
+        
+        });
+      })
+      .catch(function (e) {
+        console.error(e);
+      });
+  }
+  function loadDetails(item) {
+    let url = item.detailsUrl;
+    return fetch(url).then(function (response) {
+      return response.json();
+    }).then(function (details) {
+      // Now we add the details to the item
+      item.imageUrl = details.sprites.front_default;
+      item.height = details.height;
+      item.types = [];
+      details.types.forEach(function(typeItem){
+        item.types.push(typeItem.type.name)
+      })
+    }).catch(function (e) {
+      console.error(e);
+    });
+  }
 
-    function showDetails(pokemon){
-      console.log(pokemon)
-    }
-    return {
-      add: add,
-      getAll: getAll,
-      addlistitem: addlistitem
-    };
-  })();
+  return {
+    add: add,
+    getAll: getAll,
+    addlistitem: addlistitem,
+    loadList: loadList,
+    loadDetails: loadDetails
+  };
+})();
 
-  console.log(pokemonRepository.getAll())
-pokemonRepository.add({ name: "Charizd", height: 15, types: ["flying", "fire"] })
-console.log(pokemonRepository.getAll())
+pokemonRepository.loadList().then(function () {
+  pokemonRepository.getAll().forEach(function (pokemon) {
+    // let color;
+    // if (pokemon.types.includes("fire")) {
+    //   color = '<span style="color: red;"> ';
+    // } else if (pokemon.types.includes("water")) {
+    //   color = '<span style="color: blue;"> ';
+    // } else if (pokemon.types.includes("speed")) {
+    //   color = '<span style="color: orange;"> ';
+    // } else if (pokemon.types.includes("flying")) {
+    //   color = '<span style="color: lightblue;"> ';
+    // } else if (pokemon.types.includes("grass")) {
+    //   color = '<span style="color: green;"> ';
+    // }
 
-pokemonRepository.getAll().forEach(function(pokemon){
-    let color;
-    if(pokemon.types.includes("fire")){
-        color='<span style="color: red;"> '
-    } else if(pokemon.types.includes("water")){
-        color='<span style="color: blue;"> ' 
-    } else if(pokemon.types.includes("speed")){
-        color='<span style="color: orange;"> ' 
-    } else if(pokemon.types.includes("flying")){
-        color='<span style="color: lightblue;"> '
-    } else if(pokemon.types.includes("grass")){
-        color='<span style="color: green;"> '
-    }
-
-    let size = "";
-    if (pokemon.height > 15) {
-      size = "This Is A Tall Pokemon";
-    } else if (pokemon.height <= 10) {
-      size = "This Is A Short Pokemon";
-    } else {
-      size = "This Is An Average Pokemon";
-    }
-    pokemonRepository.addlistitem(pokemon)
-})
-  
+    // let size = "";
+    // if (pokemon.height > 15) {
+    //   size = "This Is A Tall Pokemon";
+    // } else if (pokemon.height <= 10) {
+    //   size = "This Is A Short Pokemon";
+    // } else {
+    //   size = "This Is An Average Pokemon";
+    // }
+    pokemonRepository.addlistitem(pokemon);
+  });
+});
